d46ef72299994d81158fae165d91eb6c2b7ebf19
João Victor <joaovictor.zuca12@gmail.com>
2026-08-07 23:42:06 -0300

    Create: pipeline

---
+++ src/pipeline.py
@@ -0,0 +1,28 @@
+from pathlib import Path
+from src.extract import extract
+from src.transform import transform
+from src.load import load
+from src.connect.connection import con, closeDB
+
+try:
+    path_root = Path(__file__).resolve().parent.parent
+except NameError:
+    path_root = Path.cwd()
+
+def pipeline():
+
+    conn = con()
+
+    try:
+        
+        dfs,errors = extract(path_root)
+        dfs = transform(dfs)
+        load(dfs, conn)
+
+    finally:
+        closeDB(conn)
+        print('Finalized ETL')
+
+
+if __name__ == "__main__":
+    pipeline()
